fdd025a0953d8b53991423f6c417d7673d644d03
jakisamaurice <jakisamaurice@gmail.com>
2025-07-06 16:42:59 +0300

    completed exercise1.1

--- multiply.js
+++ multiply.js
@@ -1,5 +1,33 @@
-function multiply(a, b){
-  return a * b;
-}
+// function multiply(a, b){
+//   return a * b;
+//   // return 1;
+// }
+// export default multiply;
+
+// export default multiply;
+// // function multiply(a, b){
+// //   return 1;
+// // }
+
+// export default multiply;
+// // function multiply(a, b){
+// //   return 1;
+// // }
+
+// function multiply(a, b){
+//   if ((a, b) == (1, 1)){
+//     return 1;
+//   }    
+//   if ((a, b) == (2, 2)){
+//     return 4;
+//   }  
+//   else{
+//     return 0 
+//   }
+
+// }
 
 export default multiply;
+function multiply(a, b) {
+  return a * b;
+}
--- test.js
+++ test.js
@@ -20,11 +20,22 @@ describe('multiply()', function(){
   it('should return 1 when multiplying 1 and 1', function(){
     assert.equal(multiply(1, 1), 1)
   })
-})
-
-describe('multiply()', function(){
   it('should return 4 when multiplying 2 and 2', function(){
     assert.equal(multiply(2, 2), 4)
   })
+  it('should return 9 when multiplying 3 and 3', function(){
+    assert.equal(multiply(3, 3), 9)
+    
+  })
+  it('should return 16 when multiplying 4 and 4', function(){
+    assert.equal(multiply(4, 4), 16)
+    
+  })
+  it('should return 23 * 45 when multiplying 23 and 45', function(){
+    assert.equal(multiply(23, 45), 23 * 45)
+    
+  })
 })
-// assert.equal(multiply(2, 2),4);
+
+
+// assert.equal(multiply(3, 3), 9);
