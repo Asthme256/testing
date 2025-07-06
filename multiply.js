afb5cc5033812a51dcfeb8447506d0133525175d
Kaasa Asthme <donnydonnie1@gmail.com>
2025-07-06 17:01:02 +0300

    change

--- multiply.js
+++ multiply.js
@@ -1,5 +1,43 @@
-function multiply(a, b){
+// export default multiply;
+// // function multiply(a, b){
+// //   return 1;
+// // }
+
+// this passes only test 1
+// export default multiply;
+// // function multiply(a, b){
+// //   return 1;
+// // }
+
+// this passes tests 1 and 2 only
+// function multiply(a, b){
+//   if ((a, b) == (1, 1)){
+//     return 1;
+//   }    
+//   if ((a, b) == (2, 2)){
+// //     return 4;
+// //   }  
+// //   else{
+// //     return 0 
+// //   }
+
+// // This passes test 3 and fails test 1 and 2
+// function multiply(a, b) {
+//   if((a,b) == (3,3)){
+//     return 9;
+//   }
+//   else{
+//     return 0
+//   }
+// }
+
+
+// This passes all 3 tests
+export default multiply;
+function multiply(a, b) {
   return a * b;
 }
 
-export default multiply;
+
+    
+    
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+// By: Mungu Jakisa Maurice
+//     Kaasa Asthme
+
+
 // eslint-disable @typescript-eslint/no-require-imports
 // eslint-disable no-undef
 // function multiply(a, b){
@@ -17,6 +21,12 @@ const multiply = require('./multiply.js').default;
 describe('multiply()', function(){
   it('should return 1 when multiplying 1 and 1', function(){
     assert.equal(multiply(1, 1), 1)
-  })
+  });
+  it('should return 4 when multiplying 2 and 2', function(){
+    assert.equal(multiply(2, 2), 4)
+  });
+  it('should return 9 when multiplying 3 and 3', function(){
+    assert.equal(multiply(3, 3), 9)
+  });
 })
  
