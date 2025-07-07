5462ad5353b278de84e91dc420565be80cef7af6
Kaasa Asthme <donnydonnie1@gmail.com>
2025-07-07 12:34:22 +0300

    Your descriptive commit message

--- multiply.js
+++ multiply.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-// export default multiply;
-// // function multiply(a, b){
-// //   return 1;
-// // }
-
-// this passes only test 1
-// export default multiply;
-// // function multiply(a, b){
-// //   return 1;
-// // }
-
-// this passes tests 1 and 2 only
-// function multiply(a, b){
-//   if ((a, b) == (1, 1)){
-//     return 1;
-//   }    
-//   if ((a, b) == (2, 2)){
-// //     return 4;
-// //   }  
-// //   else{
-// //     return 0 
-// //   }
-
-// // This passes test 3 and fails test 1 and 2
-// function multiply(a, b) {
-//   if((a,b) == (3,3)){
-//     return 9;
-//   }
-//   else{
-//     return 0
-//   }
-// }
-
-
-// This passes all 3 tests
-export default multiply;
-function multiply(a, b) {
-  return a * b;
-}
-
-
-    
-    
-=======
 // function multiply(a, b){
 //   return a * b;
 //   // return 1;
@@ -76,4 +31,3 @@ export default multiply;
 function multiply(a, b) {
   return a * b;
 }
->>>>>>> 0fd4b815eaa78f0dfb3171e8e120c292fed17a78
--- test.js
+++ test.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-// By: Mungu Jakisa Maurice
-//     Kaasa Asthme
-
-
-=======
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable no-undef */
->>>>>>> 0fd4b815eaa78f0dfb3171e8e120c292fed17a78
 // eslint-disable @typescript-eslint/no-require-imports
 // eslint-disable no-undef
 // function multiply(a, b){
@@ -24,27 +17,17 @@ const {assert} = require('chai');
 const multiply = require('./multiply.js').default;
 
 describe('multiply()', function(){
-  it('should return 1 when multiplying 1 and 1', function(){
+  it('multiply(1, 1) = 1', function(){
     assert.equal(multiply(1, 1), 1)
-<<<<<<< HEAD
-  });
-  it('should return 4 when multiplying 2 and 2', function(){
-    assert.equal(multiply(2, 2), 4)
-  });
-  it('should return 9 when multiplying 3 and 3', function(){
-    assert.equal(multiply(3, 3), 9)
-  });
-  
-=======
   })
-  it('should return 4 when multiplying 2 and 2', function(){
+  it('multiply(2, 2) = 4', function(){
     assert.equal(multiply(2, 2), 4)
   })
-  it('should return 9 when multiplying 3 and 3', function(){
+  it('multiply(3, 3) = 9', function(){
     assert.equal(multiply(3, 3), 9)
     
   })
-  it('should return 16 when multiplying 4 and 4', function(){
+  it('multiply(4, 4) = 16', function(){
     assert.equal(multiply(4, 4), 16)
     
   })
@@ -52,7 +35,6 @@ describe('multiply()', function(){
     assert.equal(multiply(23, 45), 23 * 45)
     
   })
->>>>>>> 0fd4b815eaa78f0dfb3171e8e120c292fed17a78
 })
 
 
